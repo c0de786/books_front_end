c10f1bbf85b3782ea8274618e6123463c5a6c0c2
waseemwasii <se.waseem@gmail.com>
2018-06-25 01:42:06 -0700

    Minor Build Changes

--- dist/books-front-end/main.js
+++ dist/books-front-end/main.js
@@ -427,7 +427,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/waseemwasii/WebstormProjects/books_front_end/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/waseemwasii/WebstormProjects/books-front-end/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- dist/books-front-end/polyfills.js
+++ dist/books-front-end/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/waseemwasii/WebstormProjects/books_front_end/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/waseemwasii/WebstormProjects/books-front-end/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
